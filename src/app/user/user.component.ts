03ea64886776c4365fb553cc2ad7faa1bcf21148
Aayush Jha <aayushjha5@gmail.com>
2024-06-09 11:00:16 +0530

    defining components input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -11,36 +8,21 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 
-/* managing state using angular change detection mechanism */
-/* export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
 
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
 
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
-} */
-
-/* managing state using signals */
 
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-//using computed arrow function for getting imagepath in case of signal
-//angular auto analyze and setup an subscription to this location and when signal is received
-//only then angular checks for this location. so efficiency is increased
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  //adding INput decortator to properties
+  //this will make the property settable from outside
+  //we are setting avatar from app.component.html 
+  @Input() avatar!: string;
+  @Input() name!: string;
 
-  /*get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  } */
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
 
